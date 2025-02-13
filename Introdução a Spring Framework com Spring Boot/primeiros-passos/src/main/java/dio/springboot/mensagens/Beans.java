46dc3e8171058abf6dc8f80111ec1eef7da3f323
SaileSC <sailesantos0@gmail.com>
2025-02-13 19:04:04 -0400

    SistemaMensagem Scope prototype

---
+++ Introdução a Spring Framework com Spring Boot/primeiros-passos/src/main/java/dio/springboot/mensagens/Beans.java
@@ -0,0 +1,18 @@
+package dio.springboot.mensagens;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class Beans {
+    @Bean
+    @Scope(scopeName = "prototype")
+    public Remetente remetente(){
+        System.out.println("CRIANDO UM OBJETO REMETENTE");
+        Remetente remetente = new Remetente();
+        remetente.setEmail("noreplay@dio.com.br");
+        remetente.setNome("Digital Innovation One");
+        return remetente;
+    }
+}
